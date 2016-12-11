34ecd95ce04d27c7abbb95cc852e9adad8b01fdb
Felipe <unomais@yahoo.com.br>
2016-12-11 14:04:36 -0200

    apipath correction.

--- src/common/common.module.js
+++ src/common/common.module.js
@@ -2,8 +2,11 @@
 "use strict";
 
 angular.module('common', [])
-.constant('ApiPath', 'https://ychaikin-course5.herokuapp.com')
+.constant('ApiPath', 'https://res-server2016.herokuapp.com')
 .config(config);
+
+//
+//https://ychaikin-course5.herokuapp.com
 
 config.$inject = ['$httpProvider'];
 function config($httpProvider) {
